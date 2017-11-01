52336aa9ce6d9cfca47251fe9ad8cde73a63e6bd
IronistM <parzakonis.m@gmail.com>
2017-11-01 13:32:34 +0200

    Renamed and changed parametric URL build

---
+++ install-rstudio.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Installs the latest RStudio daily desktop build for OSX/macOS and Ubuntu(amd64)
+#
+# https://support.rstudio.com/hc/en-us/articles/203842428-Getting-the-newest-RStudio-builds
+
+set -e
+
+# Pass version you want to get, eg one of daily, preview, or stable
+VERSION=$1
+
+if [ "$VERSION" = "" ]; then
+
+        echo "D: you did not supply a VERSION! Using `latest`"
+        VERSION = "latest"
+        exit
+
+fi
+
+URL="https://www.rstudio.org/download/latest/"${VERSION}"/desktop/ubuntu64/rstudio-latest-amd64.deb"
+PACKAGE=$(basename "${URL}")
+TARGET="/tmp/${PACKAGE}"
+
+# If previous file exists (from previous partial download, for example),
+# remove it.
+if [ -f "${TARGET}" ]; then
+    echo -e "A previous package was found. Removing ${TARGET} prior to new download"
+    rm "${TARGET}"
+fi
+
+echo "Downloading ${VERSION} build from: ${URL}"
+if [ -x /usr/bin/curl ] ; then
+    curl -L -o "${TARGET}" "${URL}"
+elif [ -x /usr/bin/wget ] ; then
+    wget -O "${TARGET}" "${URL}"
+else
+    echo "Unable to obtain the RStudio package: cannot find 'curl' or 'wget'"
+    exit 1
+fi
+
+echo "Installing ${TARGET}"
+LAUNCH=""
+if [[ `whoami` != "root" ]]; then
+    LAUNCH="sudo"
+fi
+${LAUNCH} dpkg -i "${TARGET}"
+
+rm "${TARGET}"
