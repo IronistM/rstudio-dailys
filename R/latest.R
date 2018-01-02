dec1f6a1b2b98b14101cf34dffc1ec7614452eeb
IronistM <parzakonis.m@gmail.com>
2018-01-02 11:19:05 +0200

    added R download solution

---
+++ R/latest.R
@@ -0,0 +1,6 @@
+#!/usr/bin/Rscript
+
+pg <- httr::content(httr::GET("http://www.rstudio.org/download/daily/desktop/ubuntu64/"), as = "text")
+doc <- xml2::read_xml(pg)
+deb <- xml2::xml_attr(xml2::xml_find_all(doc, "//tr[@id='row0']/td/a[@href]"), "href")
+download.file(deb, method="wget", dest="rstudio-desktop-daily-amd64.deb")
